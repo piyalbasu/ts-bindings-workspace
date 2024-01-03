bf7fefbc98be880c986db7772609013d8613d3f2
Piyal Basu <pbasu235@gmail.com>
2024-01-03 15:39:11 -0500

    add semi

--- web-app/src/components/HelloWorld.vue
+++ web-app/src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
 import * as HelloContract from "bindings/hello-soroban-client";
 import * as IncrementContract from "bindings/incrementor-client";
 
